022711577a608ed10e0d33de70f6fea5e9ee2f5b
Volkan <129984150+VolkanBa@users.noreply.github.com>
2026-07-05 13:06:05 +0200

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+#Prüfen ob gleich lang sind. Wenn nicht direkt false. 
+#Prüfen ob erster Buchstabe auch im zweiten vorhanden ist. 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = "".join(sorted(s))
+        t = "".join(sorted(t))
+        if len(s) != len(t):
+            return False
+        if s == t:
+            return True
+        return False
